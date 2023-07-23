60493e3ec0db0455d84a0a25dac1eb4393c0d864
Vijay Kumar <vijay.kumar.vrx@gmail.com>
2023-07-23 12:40:29 +0530

    info

--- design-patterns/design-patterns/src/main/java/com/vrx/designpatterns/DesignPatternsApplication.java
+++ design-patterns/design-patterns/src/main/java/com/vrx/designpatterns/DesignPatternsApplication.java
@@ -9,5 +9,19 @@ public class DesignPatternsApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DesignPatternsApplication.class, args);
 	}
+	/*
+	 * Design Patterns
+	 * ---------------------------
+	 * Design patterns are well proved solution of commonly occuring problems in
+	 * software design.
+	 * Design patterns represent an idea.
+	 * 
+	 * Categorization of Design Patterns
+	 * --------------------------------
+	 * 1. Creational Design Patterns : represents how to create objects
+	 * 2. Structural DEsign Patterns :
+	 * 3.
+	 *
+	 */
 
 }
